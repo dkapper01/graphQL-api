9ba767b485b3409b21dada04132eedd69b5e4047
Daniel Kapper <dkapper01@gmail.com>
2018-08-29 10:42:37 -0400

    add mongoose models author.js and book.js in models folder

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb://dan:password1@ds123822.mlab.com:23822/dan');
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
+})
 
 app.use('/graphql', graphqlHTTP({
   schema,
